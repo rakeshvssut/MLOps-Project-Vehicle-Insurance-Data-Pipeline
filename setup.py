a0b60d1477fc6bfca48c493b7fa46bcdb633a43c
rakeshvssut <rakesh.vssut2022@gmail.com>
2025-12-27 20:05:16 +0530

    Project Structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rakesh Kumar Sahu",
+    author_email="rakesh.vssut2022@gmail.com",
+    packages=find_packages()
+)
